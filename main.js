2689d9db872921b2a4a32378080e05801288124b
unknown <sergio_silvalima@hotmail.com>
2021-07-07 17:59:15 -0300

    novas sections

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+// ABRE E FECHA MENU
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
     element.addEventListener('click', function () {
         nav.classList.toggle('show')
+    })
+}
+
+// CLICAR ITEM MENU E ESCONDER MENU
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function () {
+        nav.classList.remove('show')
     })
 }
