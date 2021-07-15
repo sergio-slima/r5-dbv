1209f2bf276e1d7685b0840cc1c410a50b5130d8
unknown <sergio_silvalima@hotmail.com>
2021-07-15 17:58:48 -0300

    finalizando contact

--- main.js
+++ main.js
@@ -28,3 +28,32 @@ windows.addEventListener('scroll', function () {
         header.classList.remove('scroll')
     }
 })
+
+// TESTIMONIALS CARROSEL 
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true
+})
+
+// SCROLLREVEAL: MOSTRAR ELEMENTOS QUANDO DER SCROLL NA PÁGINA
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `
+    #home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links
+    `,
+    { interval: 100 }
+)
